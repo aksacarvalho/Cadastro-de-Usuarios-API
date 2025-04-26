c29def9a83358a6d74b32f6eb9448bc6324d3626
SamuelInocencio <samuelferreirainocencio@gmail.com>
2025-04-26 11:01:59 -0300

    Commit made by Samuel.

--- server.js
+++ server.js
@@ -1,33 +1,57 @@
 import express from "express";
 import { PrismaClient } from "@prisma/client";
+import cors from 'cors'
 
 const prisma = new PrismaClient();
 
 const app = express()
 app.use(express.json())
+app.use(cors());
 
 const users = []
 
 
-app.get('/usuarios/', (req, res) => {
-     
-     res.status(200).json(users)
+app.get('/usuarios', async (request, response) => {
+  const users = await prisma.user.findMany();
 
-})
+  response.status(200).json(users);
+});
 
-app.post('/usuarios', async (req, res) => {
-   
-  await prisma.user.create({
-      data: {
-         email: req.body.email,
-         age: req.body.age,
-         name: req.body.name
-      }
-    })
-  
-  res.status(201).json({ mensage: "usuario criado com sucesso"})
+app.post('/usuarios', async (request, response) => {
+  const user = await prisma.user.create({
+    data: {
+      name: request.body.name,
+      email: request.body.email,
+      age: request.body.age,
+    },
+  });
+  response.status(201).json(user);
+});
 
-})
+app.put('/usuarios/:id', async (request, response) => {
+  const user = await prisma.user.update({
+    where: {
+      id: request.params.id,
+    },
+    data: {
+      name: request.body.name,
+      email: request.body.email,
+      age: request.body.age,
+    },
+  });
+
+  response.status(200).json(user);
+});
+
+
+app.delete('/usuarios/:id', async (request, response) => {
+  const user = await prisma.user.delete({
+    where: {
+      id: request.params.id,
+    },
+  });
+  response.status(200).json(user);
+});
 
 app.listen(3005);
 // req - requisição
