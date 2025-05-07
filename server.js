5bde9068b8ac0369305e4ab4afd01725e224bf79
{aksa_carvalho} <{aksacarvalho43416@gmail.com}>
2025-05-07 14:44:51 -0300

    cors

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from 'cors'
 import { PrismaClient } from "@prisma/client";
 import cors from 'cors';
 
@@ -6,7 +7,7 @@ const prisma = new PrismaClient();
 
 const app = express()
 app.use(express.json())
-app.use(cors());
+app.use(cors())
 
 const users = []
 
